4bb9edd122045c250931f89bd75b325376a773cc
gurdjieff <515636844@qq.com>
2014-03-21 22:45:39 +0800

    gurd

---
+++ Vbill/Vbill/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Vbill
+//
+//  Created by daiyuzhang on 14-3-21.
+//  Copyright (c) 2014年 daiyuzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
